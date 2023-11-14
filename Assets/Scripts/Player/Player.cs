1b0676e4be269f44ad4f3a374d428b634655c1c2
Renan Eiji Ozima <renanozima@gmail.com>
2023-11-14 05:13:17 -0300

    meu primeiro commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public Vector2 velocity;
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
+            //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
+            //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
+        }
+    }
+}
